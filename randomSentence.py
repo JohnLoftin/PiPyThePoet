cd3d2d504bf5626cabfd0a22f61c7234ab4fc9df
JohnLoftin <jgloftin@gmail.com>
2015-07-09 00:10:09 -0400

    Updated - Now includes scraper-generated sentence pool

--- randomSentence.py
+++ randomSentence.py
@@ -1,15 +1,17 @@
 # Returns random sentences to PiPy's main logic script
 
 import random
+import urllib2
 
-sentencePool = [line.rstrip('\n') for line in open('randomSentences.txt')]
+sentencePoolOne = [line.rstrip('\n') for line in open('randomSentences.txt')]
+sentencePoolTwo = [line.rstrip('\n') for line in urllib2.urlopen("http://scraper.reppardwalker.com/sentences.txt")]
 
 def main():
 
     run = True
     
     while run:
-        totalPost = [random.choice(sentencePool), random.choice(sentencePool), random.choice(sentencePool)]
+        totalPost = [random.choice(sentencePoolOne), random.choice(sentencePoolTwo), random.choice(sentencePoolOne)]
         joinedPost = ' '.join(totalPost)
         charCount = len(joinedPost)
     
