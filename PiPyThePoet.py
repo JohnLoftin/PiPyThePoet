8f68d4dbbaa24ec24f54d10d30430a9bd38f996c
JohnLoftin <jgloftin@gmail.com>
2015-07-08 20:38:59 -0400

    PiPy's logic/posting script

---
+++ PiPyThePoet.py
@@ -0,0 +1,21 @@
+# PiPyThePoet - Twitter Bot
+
+from twython import Twython, TwythonError
+import time
+import randomSentence
+
+KEYS = [line.rstrip('\n') for line in open('keys.txt')]
+
+# PiPy's twitter keys/authentification tokens
+APP_KEY = KEYS[0]
+APP_SECRET = KEYS[1]
+OAUTH_TOKEN = KEYS[2]
+OAUTH_TOKEN_SECRET = KEYS[3]
+
+twitter_api = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+# Sets post content -> return value of randomSentence.py
+status = randomSentence.main()
+
+# Post content - Twitter
+twitter_api.update_status(status=status)
--- randomSentence.py
+++ randomSentence.py
@@ -1,3 +1,5 @@
+# Returns random sentences to PiPy's main logic script
+
 import random
 
 sentencePool = [line.rstrip('\n') for line in open('randomSentences.txt')]
@@ -5,12 +7,14 @@ sentencePool = [line.rstrip('\n') for line in open('randomSentences.txt')]
 def main():
 
     run = True
+    
     while run:
         totalPost = [random.choice(sentencePool), random.choice(sentencePool), random.choice(sentencePool)]
-        charCount = len(totalPost)
+        joinedPost = ' '.join(totalPost)
+        charCount = len(joinedPost)
     
         if charCount <= 140:
-            return ' '.join(totalPost)
+            return joinedPost
             run = False
 
 
