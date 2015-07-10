d1fbf1e7dcedb29cac90b59bf2fbfc0b0f7d0f45
JohnLoftin <jgloftin@gmail.com>
2015-07-09 23:10:05 -0400

    Changed sentencePool - Now generates random sample list (50 lns) - no longer stores array of entire .txt

--- randomSentence.py
+++ randomSentence.py
@@ -3,15 +3,27 @@
 import random
 import urllib2
 
-sentencePoolOne = [line.rstrip('\n') for line in open('randomSentences.txt')]
-sentencePoolTwo = [line.rstrip('\n') for line in urllib2.urlopen("http://scraper.reppardwalker.com/sentences.txt")]
+#########################################################################################################
+#                                                                                                       #
+#                       !!! Placeholder for when I get Scrapy scraper up !!!                            #
+#                                                                                                       #
+# sentencePoolOne = random.sample(list(line.rstrip('\n') for line in open('randomSentences.txt')), 50)  #
+#                                                                                                       #
+#########################################################################################################
+
+
+# Pull 50 random lines from scraped sentence file - store as lines in list
+sentencePool = random.sample(list(line.rstrip('\n') for line in urllib2.urlopen("http://johnloftin.farted.net/sentences.txt")), 50)
 
 def main():
 
     run = True
     
     while run:
-        totalPost = [random.choice(sentencePoolOne), random.choice(sentencePoolTwo), random.choice(sentencePoolOne)]
+        totalPost = [random.choice(sentencePool),
+                     random.choice(sentencePool),
+                     random.choice(sentencePool)]
+        
         joinedPost = ' '.join(totalPost)
         charCount = len(joinedPost)
     
