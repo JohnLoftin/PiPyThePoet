4ac7dcf0f47aba70df4b7adcbec5a1c736683cd0
JohnLoftin <jgloftin@gmail.com>
2015-08-23 16:10:35 -0400

    Adjusted source file for sentence generation

--- randomSentence.py
+++ randomSentence.py
@@ -3,17 +3,9 @@
 import random
 import urllib2
 
-#########################################################################################################
-#                                                                                                       #
-#                       !!! Placeholder for when I get Scrapy scraper up !!!                            #
-#                                                                                                       #
-# sentencePoolOne = random.sample(list(line.rstrip('\n') for line in open('randomSentences.txt')), 50)  #
-#                                                                                                       #
-#########################################################################################################
-
 
 # Pull 50 random lines from scraped sentence file - store as lines in list
-sentencePool = random.sample(list(line.rstrip('\n') for line in urllib2.urlopen("http://johnloftin.farted.net/sentences.txt")), 50)
+sentencePool = random.sample(list(line.rstrip('\n') for line in open('randomSentences.txt')), 50)
 
 def main():
 
